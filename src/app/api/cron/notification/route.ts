6730e076d2aa576936da4c19f0d423017c46b18d
吉村直人 <ahowateto123@gmail.com>
2025-08-25 23:49:37 +0900

    通知APIの認証をコメントアウト

--- src/app/api/cron/notification/route.ts
+++ src/app/api/cron/notification/route.ts
@@ -93,9 +93,9 @@ export async function GET(request: NextRequest) {
   const logPrefix = "[API: GET /api/notification]"
 
   try {
-    if (!authenticate(request)) {
-      throw new CustomError(ERROR_CODE.UNAUTHORIZED)
-    }
+    // if (!authenticate(request)) {
+    //   throw new CustomError(ERROR_CODE.UNAUTHORIZED)
+    // }
 
     const { teams, standings, games } = await fetchMlbData()
 
