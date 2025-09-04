d49b43fa4dc717d7065baa33e45292607aac23b9
吉村直人 <ahowateto123@gmail.com>
2025-09-04 18:25:20 +0900

    チームカード: アクティブ状態の不透明度を0.85から0.87に変更

--- src/features/teams/components/team-card.tsx
+++ src/features/teams/components/team-card.tsx
@@ -29,7 +29,7 @@ export default function TeamCard({
         border: "3px solid",
         borderColor: isActive ? "primary.main" : "white",
         bgcolor: "white",
-        opacity: isActive ? 0.85 : 1,
+        opacity: isActive ? 0.87 : 1,
       }}
     >
       <CardContent
