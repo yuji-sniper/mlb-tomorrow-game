ad60f7114cee437e222f48808a9ae5c082734fff
吉村直人 <ahowateto123@gmail.com>
2025-09-08 01:54:50 +0900

    middleware: レートリミット対応

---
+++ src/middleware.ts
@@ -0,0 +1,42 @@
+import { Ratelimit } from "@upstash/ratelimit"
+import { kv } from "@vercel/kv"
+import { type NextRequest, NextResponse } from "next/server"
+
+const RATE_LIMIT_COUNT = 10
+const RATE_LIMIT_DURATION = "10s"
+
+export async function middleware(request: NextRequest) {
+  // レートリミット
+  const rateLimitSuccess = await rateLimit(request)
+  if (!rateLimitSuccess) {
+    return new NextResponse("Too Many Requests", { status: 429 })
+  }
+
+  return NextResponse.next()
+}
+
+/**
+ * レートリミット
+ */
+async function rateLimit(request: NextRequest) {
+  if (process.env.NODE_ENV !== "production") {
+    return true
+  }
+
+  const forwardedFor = request.headers.get("x-forwarded-for")
+  const ip =
+    forwardedFor?.split(",")[0]?.trim() ??
+    request.headers.get("x-real-ip") ??
+    "127.0.0.1"
+
+  const rateLimit = new Ratelimit({
+    redis: kv,
+    limiter: Ratelimit.slidingWindow(RATE_LIMIT_COUNT, RATE_LIMIT_DURATION),
+  })
+
+  const { success } = await rateLimit.limit(ip)
+
+  console.log(ip, success)
+
+  return success
+}
