f0ef6e395f98c1953eed44c6904846cfc26b3ed7
吉村直人 <ahowateto123@gmail.com>
2025-09-11 07:59:00 +0900

    画像の許可ドメインを設定

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.mlbstatic.com",
+      },
+      {
+        protocol: "https",
+        hostname: "www.mlbstatic.com",
+      }
+    ],
+  },
 };
 
 export default nextConfig;
